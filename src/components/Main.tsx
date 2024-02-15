9674ace5b4ecf49b00e7c5e95e41d82f77491e10
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-02-15 15:16:25 +0100

    add header

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,29 @@
 import React from "react";
 import "../styles/components/main.scss";
+import Svg from "../assets/Svg";
+import { search } from "../static/icons";
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="maincontainer">
+      <Header />
+    </div>
+  );
+}
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src="/logo.png" alt="logo" />
+        <div className="header__logo__text">MUUUSIC</div>
+      </div>
+      <div className="header__search">
+        <input type="text" placeholder="Search for songs, artists, albums" />
+        <Svg
+          path={search.path}
+          view={search.view}
+          classlist="header__search__icon"
+        />
+      </div>
+    </div>
+  );
 }
